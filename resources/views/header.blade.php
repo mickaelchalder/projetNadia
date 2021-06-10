4e36b6c7e9127a685b4d7e5a413afe796d9c0dd5
mickael <chalder@hotmail.fr>
2021-06-10 11:17:33 +0200

    création fichier blade & css deu header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/header.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <a href="index.html">logo</a>
+        </div>
+        <div class="logo">
+            <a href="index.html">Event</a>
+        </div>
+        <div class="logo">
+            <a href="index.html">Article</a>
+        </div>
+        <div class="logo">
+            <a href="index.html">Produit&Prestation</a>
+        </div>
+        <div class="logo">
+            <a href="index.html">Newsletter</a>
+        </div>
+        <div class="logo">
+            <a href="index.html">Login</a>
+        </div>
+        
+    </nav>
+</body>
+</html>
