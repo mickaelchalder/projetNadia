17157416ea2ca7e495b0c9086938aafa1f7c3dfb
mickael <chalder@hotmail.fr>
2021-06-26 11:14:02 +0200

    css mofifier event ok

--- resources/views/modifEvent.blade.php
+++ resources/views/modifEvent.blade.php
@@ -1,56 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Events</title>
-</head>
-<body>
+@include('layout.header')
+
     <h1>modifier l'article du {{$modif->date}}</h1>
+    <div class="article">
+        <form method="post" action="{{route("modifEvent")}}" enctype="multipart/form-data">
+            <fieldset>
+            <legend>modifier la date </legend>
+                @csrf
+                {!! method_field('PUT') !!}
+                <input type="date" name="newDate" value="{{$modif->date}}" class="titreArticle">
+            </fieldset>
 
-    <form method="post" action="{{route("modifEvent")}}" enctype="multipart/form-data">
-        <fieldset>
-        <legend>modifier la date </legend>
-            @csrf
-            {!! method_field('PUT') !!}
+            <fieldset>
+            <legend>modifier la titre </legend>
 
-            <label for="bday">Veuillez saisir la date de l'event :</label>
-            <input type="date" name="newDate" value="{{$modif->date}}">
-        </fieldset>
+                <label for="titre">Titre de l'article : </label><input type="text"  name="newTitre" value="{{$modif->titre}}" class="titreArticle">
+            </fieldset>
 
-        <fieldset>
-        <legend>modifier la titre </legend>
+            <fieldset>
+                <legend>modifier l'image</legend>
+                
+                <input type="hidden" name="actualImg" value="{{$modif->img}}">
+                <img src={{ asset('storage/image/'.$modif->img) }}>
+                <input type="file" name="newImg" class="fileColor"   class="cadreImage2">
+            </fieldset>
 
-            <label for="titre">Titre de l'article : </label><input type="text"  name="newTitre" value="{{$modif->titre}}" >
-        </fieldset>
+            <fieldset>
+            <legend>modifier la message </legend>
 
-        <fieldset>
-            <legend>modifier l'image</legend>
-            
-            <input type="hidden" name="actualImg" value="{{$modif->img}}">
-            <img src={{ asset('storage/image/'.$modif->img) }}>
-            <input type="file" name="newImg" class="fileColor"  >
-        </fieldset>
+                <textarea type="textarea" name="newMessage" class="message">>{{$modif->message}}</textarea>
+            </fieldset>
 
-        <fieldset>
-        <legend>modifier la message </legend>
+            <input type="hidden" name="id" value="{{ $modif->id }}">
 
-            <textarea type="textarea" name="newMessage" >{{$modif->message}}</textarea>
-        </fieldset>
-
-        <input type="hidden" name="id" value="{{ $modif->id }}">
-
-            <input type="submit" name="send" value="Valider">
-            <input type="reset">
-        </form>
-    
-        <form action='{{ url('event') }} ' method='post' >
-            @csrf
-            <input type='hidden' name='date' value='{{$fecha}}'>
-            <input  type='submit' value='retour' >
-        </form>
-    
-</body>
-</html>
+                <input type="submit" name="send" value="Valider">
+                <input type="reset">
+            </form>
+        
+            <form action='{{ url('event') }} ' method='post' >
+                @csrf
+                <input type='hidden' name='date' value='{{$fecha}}'>
+                <input  type='submit' value='retour' >
+            </form>
+    </div>
+   
+    @include('layout.footer')
 
