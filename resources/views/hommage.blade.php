2b8b35345df857fea200e6f6164e080689696f03
mickael <chalder@hotmail.fr>
2021-06-30 10:33:13 +0200

    css ok fin quasi fin du site

--- resources/views/hommage.blade.php
+++ resources/views/hommage.blade.php
@@ -1,34 +1,74 @@
 @include('layout.header')
-
 <div class="background-hommage">
 
-<form action="{{ url('modifHommage') }}" method="POST" enctype="multipart/form-data">
+	@auth
+	<h1 class="h1-hommage">Ajouter un hommage</h1>
 
-{{ csrf_field() }}
-{!! method_field('PUT') !!}
-<input type="hidden" name="id" value="{{ $hommageModif->id }}">
-    <h1 class="titre-hommage">Hommage</h1>
-    <div class="pc-hommage">
-        <div>
-        @error('titre')
-            <div class="invalid-feedback">{{ $message }}</div>
-        @enderror
-        <input type ='text' value='{{$hommageModif->titre}}' name="titre" class="titre-hommage"></input>
-        @error('message')
-                <div class="invalid-feedback">{{ $message }}</div>
-        @enderror
-        </div><br>
+		<form method="post" action="{{url("addHommage")}}" enctype="multipart/form-data">
+
+		{{csrf_field()}}
+
+<label for="titre">Titre de l'hommage : </label>
+			@error('titre')
+            	<div class="invalid-feedback-hommage">{{ $message }}</div>
+        	@enderror
+        	<input type="text" id="titre" name="titre" value="{{ old('titre') }}">
+
+
         @error('img')
-                <div class="invalid-feedback">{{ $message }}</div>
+            <div class="invalid-feedback-hommage">{{ $message }}</div>
         @enderror
-            <img src="{{ asset('storage/image/'.$hommageModif->img) }}" class="img-hommage"><br>
-            <input type="hidden" name="img2" value="{{$hommageModif->img}}"/>
-            <input type="file" name="img"/><br>
-            <textarea type="text" name="message" class="message-hommage">{{$hommageModif->message}}</textarea>
-            <br>
-        <div><input type="submit" name="modif" value="Modifier"></div>
-        <div><a href="{{ route('listeH') }}" class="lien-color">retour</a></div>
+		<span><input type="file" name="img"></span>
+		<span><div><label for="message">Message : </label></div>
+			@error('message')
+                <div class="invalid-feedback-hommage">{{ $message }}</div>
+            @enderror
+			<textarea type="text" name="message" cols="40" rows="20" >{{ old('message') }}</textarea></span>
+		<input type="submit" name="send" value="Valider">
+		<input type="reset">
 
-    </div>
-</form>
+	</form>
+	@endauth
+
+<div class="div-back"><a href="{{url('/')}}" class="div-back-btn">&times;</a></div>
+
+	@auth
+		@foreach($hommages as $hommage)
+		
+		<form action="{{url('hommages/'.$hommage->id)}}" method="post">
+				{{csrf_field()}}
+				{!!method_field('DELETE')!!}
+		<div class="pc-hommage">
+				<input type="hidden" name="id" value="{{$hommage->id}}">
+				<img src="{{ asset('storage/image/'.$hommage->img) }}" class="img-hommage">
+						<h2 class="titre-hommage">{{$hommage->titre}}</h2>
+						<div class="message-hommage">{!!nl2br($hommage->message)!!}</div>
+		</div>
+
+		<button>Supprimer</button>
+			</form>
+
+		<form action="{{ url('hommages/'.$hommage->id) }}" method="GET">
+            <div><button>
+                Modifier
+            </button></div>			
+        </form>
+
+        <div class="container-flex-container-liseret"><img src="{{asset('storage/image/liseret_hommage.png')}}" class="liseret_hommage"></div>
+		@endforeach
+	@endauth
+
+	@guest
+		@foreach($hommages as $hommage)
+		<div class="pc-hommage">
+		<img src="{{ asset('storage/image/'.$hommage->img) }}" class="img-hommage">
+		<h2 class="titre-hommage">{{$hommage->titre}}</h2>
+		<div class="message-hommage">{!!nl2br($hommage->message)!!}</div>
+		</div>
+		<div class="container-flex-container-liseret"><img src="{{asset('storage/image/liseret_hommage.png')}}" class="liseret_hommage"></div>
+		
+		@endforeach
+	@endguest
+
 </div>
+@include('layout.footer')
